8ea4136dd32c592446fe6e16a324fd3c51818284
sandra2022 <mca544@rajagiri.edu>
2021-03-17 14:05:20 +0530

    assignment3

---
+++ aboutus.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+        img {
+          display: block;
+          margin-left: auto;
+          margin-right: auto;
+        }
+        </style>
+        </head>
+        <body>
+        
+        <h2>more to know..</h2>
+       
+        
+        <img src="trv.jfif" style="width:50%;">
+        
+        </body>
+    <p><b><i>“Travel is the main thing you purchase that makes you more extravagant”. We, at ‘Organization Name’, swear by this and put stock in satisfying travel dreams that make you perpetually rich constantly</i></b></p>
+<p><b><i>We have been moving excellent encounters for a considerable length of time through our cutting edge planned occasion bundles and other fundamental travel administrations. We rouse our clients to carry on with a rich life, brimming with extraordinary travel encounters.</i></b>
+</p>
+<p><b><i>Through our exceptionally curated occasion bundles, we need to take you on an adventure where you personally enjoy with the stunning magnificence of America and far off terrains. We need you to observe sensational scenes that are a long ways past your creative ability.</i>
+
+</b></p>
+<p><b><i>The powerful inclination of American voyagers to travel more nowadays is something that keeps us inspired to satisfy your vacation necessities. Our vision to give you a consistent occasion encounter makes us one of the main visit administrators in the regularly extending travel industry.
+
+</i></b></p>
+
+
+
+</body>
+</html>
---
+++ proj3.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+        
+body {
+  background-color: lightblue;
+}
+
+        body {font-family: Arial, Helvetica, sans-serif;}
+        * {box-sizing: border-box;}
+        
+        .bg-img {
+          /* The image used */
+          background-image: url("ma3.jpg");
+        
+          height: 500px;
+          
+        
+          /* Center and scale the image nicely */
+          background-position: center;
+          background-repeat: no-repeat;
+          background-size: cover;
+          
+          /* Needed to position the navbar */
+          position: relative;
+        }
+        
+        /* Position the navbar container inside the image */
+        .container {
+          position: absolute;
+          margin: 20px;
+          width: auto;
+        }
+        
+        /* The navbar */
+        .topnav {
+          overflow: hidden;
+          background-color: #333;
+        }
+        
+        /* Navbar links */
+        .topnav a {
+          float: left;
+          color: #f2f2f2;
+          text-align: center;
+          padding: 14px 16px;
+          text-decoration: none;
+          font-size: 17px;
+        }
+        
+        .topnav a:hover {
+          background-color: #ddd;
+          color: black;
+        }
+
+        </style>
+        </head>
+        <body>
+        
+        <h2>Travelogue</h2>
+        <div class="bg-img">
+          <div class="container">
+            
+            <div class="topnav">
+              <a href="tripplanner.html">Trip Planner</a>
+              
+              <a href="contactus.html">Contact Us</a>
+              <a href="aboutus.html">About Us</a>
+             
+            </div>
+          </div>
+        </div>
+        <style>
+            .centered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+        </style>
+        <div class="container"></div>
+        <div class="centered"><h1><b><i>DON'T TRAVEL FOR CHECK-INS OR PICS
+            TRAVEL WITHOUT ANY REASON OR ALONE 
+            DO LET ME KNOW WHENEVER YOU RETURN...
+        </i></b></h1></div>
+        <h3><b>Why travel around the world?</b></h3>
+        <p><i>We believe that traveling around the world shouldn’t be hard: it’s actually something everyone should be able to do at least once in their lives. Whether you choose to spend a few years or just a couple months traveling this beautiful planet, it’s important to see what’s out there. It’s up to you to make the dream come true and take the first step. Launch TripPlanner to piece together and price your ideal route. Not sure where to start?  You can always call one of our travel consultants and get some complimentary advice!</i></p>
+        <style>
+            img {
+              display: block;
+              margin-left: auto;
+              margin-right: auto;
+            }
+            </style>
+            </head>
+            <body>
+            
+            <h2>Traveling is easier than you think.</h2>
+            
+            <img src="wt.jfif"  style="width:50%;">
+            <p>If you’re open and willing, travel will make you an incredibly more well-rounded human being. And that’s really the goal, isn’t it? If you don’t know where to start, check out our Around the World planning guide..</p>
+            <h2>. Traveling helps you learn who you are..</h2>
+            <img src="t1.jfif"  style="width:50%;">
+            <p>All the challenges and opportunities travel lays at your feet help you discover who you are in a way that’s only possible on the road</p>
+             <h2> Travel creates meaningful relationships</h2>
+            
+             <img src="t2.jfif"  style="width:40%;">
+             <p>People you meet while on the road become some of the most valued names on your contact list. They become places on the map to visit later on. These folks give you a glimpse outside your hometown circle of friends, and force you to take in new and refreshing perspectives, and ultimately realize that everyone is the same.</p>
+             <style> 
+                #example1 {
+                  background-image: url(t3.jfif), url(paper.gif);
+                  background-position: left bottom, left top;
+                  background-repeat: no-repeat, repeat;
+                  padding: 15px;
+                }
+                </style>
+                </head>
+                <body>
+                
+              
+                <div id="example1">
+                  <h1>Traveling develops skills you didn’t know you had</h1>
+                   <p style="color:darkorange">Sometimes it’s only far from home that you realize you you’ve got skills you’ve never used. It’s travel that brings them to the surface and makes you smile, satisfied to have reached the mountain top, or  crossed a gorge  or helped a villager clean up after a storm, or even to have successfully ordered a meal at a rural Chinese restaurant.</p>
+                   <p style="color:darkorange"> There’s something satisfying about being able to throw around a few words of Greek, knowing how to say thanks in Thai, pulling out that long dormant Spanish to book a room in Santiago, or simply hearing a language you didn’t know existed just a few weeks before.</p>
+                </div>
+   
+        </body>
+</html>
